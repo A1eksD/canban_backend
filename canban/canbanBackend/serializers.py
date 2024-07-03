cb8cefaee3fd56fbae0b127c5484ebe04c99f22e
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-07-03 14:18:52 +0200

    changed create user to get a token

--- canban/canbanBackend/serializers.py
+++ canban/canbanBackend/serializers.py
@@ -62,27 +62,41 @@ class TaskSerializer(serializers.ModelSerializer):
         
         
         
+# class UserSerializer(serializers.ModelSerializer):
+#     password = serializers.CharField(write_only=True)
+    
+#     def create(self, validated_data):
+
+#         user = User.objects.create_user(
+#             username=validated_data['username'],
+#             password=validated_data['password'],
+#         )
+
+#         return user
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email' , 'password']
+#         extra_kwargs = {'password': {'write_only': True}}
+        
+#     def create(self, validated_data):
+#         user = User.objects.create_user(
+#             username=validated_data['username'],
+#             email=validated_data['email'],
+#             password=validated_data['password']
+#         )
+#         return user
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    
-    def create(self, validated_data):
 
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-        )
-
-        return user
     class Meta:
         model = User
-        fields = ['username', 'email' , 'password']
+        fields = ['username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
-        
+
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email'),
             password=validated_data['password']
         )
         return user
-        
--- canban/canbanBackend/views.py
+++ canban/canbanBackend/views.py
@@ -21,15 +21,31 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 
+# @api_view(['POST'])
+# @permission_classes([AllowAny])
+# def register_user(request):
+#     serialized = UserSerializer(data=request.data)
+#     if serialized.is_valid():
+#         return Response(serialized.data, status=status.HTTP_201_CREATED)
+#     else:
+#         return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def register_user(request):
     serialized = UserSerializer(data=request.data)
     if serialized.is_valid():
-        return Response(serialized.data, status=status.HTTP_201_CREATED)
+        user = serialized.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        }, status=status.HTTP_201_CREATED)
     else:
-        return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     """
