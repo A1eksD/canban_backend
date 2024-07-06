a3bea67a8d30d8561b3f6a930aaf25022fc7231a
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-07-06 16:51:14 +0200

    tryed to fix  assigned_users

--- canban/canbanBackend/serializers.py
+++ canban/canbanBackend/serializers.py
@@ -25,10 +25,19 @@ class SubtaskSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'is_checked']
         
         
+
+class PublicUserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ['id','username']
+        
+        
+        
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     subtasks = SubtaskSerializer(many=True)
-    assigned_users = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
+    assigned_users = PublicUserSerializer(many=True)
 
     class Meta:
         model = Task
@@ -42,9 +51,17 @@ class TaskSerializer(serializers.ModelSerializer):
             SubTask.objects.create(task=task, **subtask_data)
         # for assigned_users in assigned_users:
         #     User.objects.create(task=task, **assigned_users)
-        task.assigned_users.set(assigned_users_data)
-        return task
+        #-----------------------------------------------------
+        # assigned_users = [User.objects.get(id=user_data['id']) for user_data in assigned_users_data]
+        # print(f'Assigned users: {assigned_users}', assigned_users)
+        # task.assigned_users.set(assigned_users)
+        #-----------------------------------------------------
+        # task.assigned_users.set([user.pk for user in assigned_users_data])
+        #-----------------------------------------------------
+        assigned_users = [PublicUserSerializer(data=user_data).save() for user_data in assigned_users_data]
+        task.assigned_users.set(assigned_users)
 
+    
     def update(self, instance, validated_data):
         subtasks_data = validated_data.pop('subtasks')
         assigned_users_data = validated_data.pop('assigned_users')
@@ -58,30 +75,9 @@ class TaskSerializer(serializers.ModelSerializer):
         for subtask_data in subtasks_data:
             SubTask.objects.create(task=instance, **subtask_data)
 
-        instance.assigned_users.set(assigned_users_data)
+        assigned_users = [User.objects.get(id=user_data['id']) for user_data in assigned_users_data]
+        instance.assigned_users.set(assigned_users)
+        #-----------------------------------------------------
+        # instance.assigned_users.set([user.pk for user in assigned_users_data])
 
         return instance
-
-
-class PublicUserSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = User
-        fields = ['id','username']
-        
-
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password']
-        )
-        return user
